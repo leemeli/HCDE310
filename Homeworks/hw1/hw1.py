671df5a4c41bc64a9df0fe07cae915ce4c924096
Sean Munson <smunson@gmail.com>
2016-10-04 14:40:28 -0700

    HW 1

---
+++ Homeworks/hw1/hw1.py
@@ -0,0 +1,25 @@
+lst = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+otherlst = ['a','b','c','d','e','f','g']
+s = "This is a test string for HCDE 310"
+
+#Exercise 1 (working with a list):
+#a.	Print the first element of lst
+
+#b.	Print the last element of otherlst
+
+#c.	Print the first five elements of lst
+
+#d.	Print the fifth element of otherlst
+
+#e.	Print the number of items in lst
+
+#Exercise 2 (working with a string):
+#a.	Print the first four characters of s
+
+#b.	Using indexing, print the substring "test" from s
+
+#c.	Print the contents of s starting from the 27th character (H)
+
+#d.	Print the last three characters of s
+
+#e.	Print the number of characters in s
