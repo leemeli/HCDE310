36fb43f0bd0a815c8547e2dbf2e225adfc3fe077
Melody Lee <leemeli@uw.edu>
2016-10-04 17:28:59 -0700

    Complete homework 1 code

--- Homeworks/hw1/hw1.py
+++ Homeworks/hw1/hw1.py
@@ -4,22 +4,32 @@ s = "This is a test string for HCDE 310"
 
 #Exercise 1 (working with a list):
 #a.	Print the first element of lst
+print lst[0]
 
 #b.	Print the last element of otherlst
+print otherlst[-1]
 
 #c.	Print the first five elements of lst
+print lst[:5]
 
 #d.	Print the fifth element of otherlst
+print otherlst[4]
 
 #e.	Print the number of items in lst
+print len(lst)
 
 #Exercise 2 (working with a string):
 #a.	Print the first four characters of s
+print s[:4]
 
 #b.	Using indexing, print the substring "test" from s
+print s[10:14]
 
 #c.	Print the contents of s starting from the 27th character (H)
+print s[26:]
 
 #d.	Print the last three characters of s
+print s[-3:]
 
 #e.	Print the number of characters in s
+print len(s)
