a68b1e36d891f3c3ea28835c082b00b405e3caeb
Melody Lee <leemeli@uw.edu>
2016-10-11 17:05:58 -0700

    Finish s4challenge.py

--- Lectures/s4/s4challenge.py
+++ Lectures/s4/s4challenge.py
@@ -15,12 +15,35 @@ wordlist = fstring.split()
 ### file for you above
 
 ### fill in your code here
+elementary = 0
+obviously = 0
+
+for word in wordlist:
+    current = word.lower()
+    current = stripWordPunctuation(current)
+    if current == "elementary":
+        elementary = elementary + 1
+    elif current == "obviously":
+        obviously = obviously + 1
+print "elementary", elementary
+print "obviously", obviously
+
 
 
 ### Challenge 1: modify the code below to print
 ### all of the words that appear more than 10 times
 ### and the number of times they appear
 ###
+d = {}
+for word in wordlist:
+    current = word.lower()
+    current = stripWordPunctuation(current)
+    d[word] = d.get(word,0)+1
+for dictWord in d:
+    if d[dictWord] > 10:
+        print dictWord, d[dictWord]
+
+
 ###
 ### Challenge 2: modify and add to your code
 ### to print the word that appears the most.
@@ -28,8 +51,21 @@ wordlist = fstring.split()
 ### Extra challenge 2: remove punctuation using 
 ###                    stripWordPunctuation()
 
-freqs = {}            #an empty dictionary to keep track of frequency
+#freqs = {}            #an empty dictionary to keep track of frequency
 # tally
+
+largest = 0
+mystring = ""
+d = {}
+for word in wordlist:
+    current = word.lower()
+    current = stripWordPunctuation(current)
+    d[word] = d.get(word,0)+1
+for dictWord in d:
+    if d[dictWord] > largest:
+        largest = d[dictWord]
+        mystring = dictWord
+print dictWord, largest
 
 # print tally
 # printing only words appearing more than 10 times
