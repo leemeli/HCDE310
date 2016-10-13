a1c0c159cd4de3e3d904ef0db8c9bd6106e257f3
Sean Munson <smunson@gmail.com>
2016-10-13 14:55:41 -0700

    lecture 5

---
+++ Lectures/s5/reviewex/reviewex.py
@@ -0,0 +1,34 @@
+### variable assignments
+# 1: add statements immediately below so that print x*y prints out 12
+# (before you do this, it will result in an error)
+
+print x*y
+
+### expressions
+# 2: determine what the following will print (after you have done #1)
+
+print 3 + x * y
+print (3 + x) * y
+
+### 3 string operations
+s = "Hello World \n"
+# write code that does the following, 
+
+# 3a: print s in all upper case
+
+# 3b: print s in all lower case
+
+# 3c: print s without an extra blank line
+
+# 3d: use slicing to extract and print "World"
+
+# 3e: write a line of code so that print subst will
+#     print "Hello" (until you write this, it will produce an error)
+
+print subst
+
+# 3f: write code that counts how many words are in s
+
+# 3g: write code to find out how many non-whitespace characters are
+#     in s (i.e, the characters that are not "\t", "\n", or " ")
+
